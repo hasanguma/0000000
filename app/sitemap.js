66fe20a845309920de0d2b3a0f3c817aa4fe2f53
hasanguma <h83797690@gmail.com>
2026-07-20 03:54:36 +0300

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,18 @@
+export default async function sitemap() {
+  const baseUrl = "https://al-dahabia.store"; 
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },
+    {
+   url: baseUrl + '/admin',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
+}
